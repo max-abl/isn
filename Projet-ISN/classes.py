59a88428c8622d86e5c6c8d4954b18d7d82894c5
Maxime Aublet <maximeaublet@gmail.com>
2015-04-05 21:31:50 +0200

    ajout spawn d'item de vie

---
+++ Projet-ISN/classes.py
@@ -0,0 +1,238 @@
+import pygame, random, graphics
+from pygame.locals import *
+
+
+#self.image = pygame.Surface((width, height))
+#self.image.fill(color)
+
+class Player(pygame.sprite.Sprite): #Classe joueur
+
+	def __init__(self, x, y, isRight, up, left, right, gun, imgR, imgL, imgFR, imgFL, jump, fire):
+		super(Player, self).__init__()
+
+		#PARAMETRES
+
+		if isRight == True: self.image = imgR
+		else: self.image = imgL
+		self.imgR = imgR
+		self.imgL = imgL
+		self.imgFR = imgFR
+		self.imgFL = imgFL
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = x, y
+		self.spawnX, self.spawnY = x, y
+		self.xspeed, self.yspeed = 0, 0
+		self.speed = 5
+		self.score = 0
+		self.life = 100
+		self.lifeBar = pygame.Surface((self.life * 2 + 1, 12))
+		self.lifeBar.fill((10, 250, 10))
+		self.jump = jump
+		self.fire = fire
+		self.isRight = isRight
+		self.up = up
+		self.left = left
+		self.right = right
+		self.gun = gun
+		self.enemy = 0
+
+	#METHODES
+
+	def setEnemy(self, enemy):
+		self.enemy = enemy
+
+	def update(self, collidable = pygame.sprite.Group()): #méthode gérant les collisions et la gravité
+
+		self.gravity() # appel de la fonction qui met en place la gravité
+
+		self.rect.x += self.xspeed # on incrémente toujours la coordonnée x de la vitesse x (de base à 0 : le joueur n'avance pas)
+
+		collisionList = pygame.sprite.spritecollide(self, collidable, False) #liste contenant toutes les collisions en train de se faire
+
+		for collided in collisionList: #On parcours la liste des collisions pour gérer l'arrêt du joueur en cas de contact horizontal
+			if self.xspeed > 0:
+				self.rect.right = collided.rect.left # : stoppe le joueur
+			if self.xspeed < 0:
+				self.rect.left = collided.rect.right # : idem
+
+		self.rect.y += self.yspeed # idem comme pour la coordonnée x
+
+		collisionList = pygame.sprite.spritecollide(self, collidable, False) # idem vertical
+
+		for collided in collisionList:
+			if self.yspeed > 0:
+				self.rect.bottom = collided.rect.top
+				self.yspeed = 0
+			if self.yspeed < 0:
+				self.rect.top = collided.rect.bottom
+				self.yspeed = 0
+
+		if self.life <= 0: # system de réaparition si le joueur n'a plus de vie
+			self.die()
+
+	def gravity(self, gravity = 1): #méthode de gravité
+		if self.yspeed == 0: # si le joueur est immobile, la gravité ne l'affecte pas
+			self.yspeed = 1
+		else:
+			self.yspeed += gravity # sinon oui
+
+	def drawLifeBar(self, screen, x, y, xx, yy, lifeBarSupport): #méthode affichage de la vie
+		if self.life <= 0:
+			self.lifeBar = pygame.Surface((0, 12)) # si la vie est inférieure à 0 on créé un rectange null
+		else:
+			self.lifeBar = pygame.Surface((self.life * 2 + 1, 12)) # sinon on crée un rectangle dont la longeur varie en fonction de la vie
+		self.lifeBar.fill((10, 250, 10))
+		screen.blit(lifeBarSupport, (x, y))
+		screen.blit(self.lifeBar, (xx, yy))
+
+	def drawScore(self, screen, x, y):
+		screen.blit(graphics.score[self.score], (x, y))
+
+	def addScore(self, number):
+		self.score += number
+
+	def addLife(self, life):
+		self.life += life
+		if self.life > 100:
+			self.life = 100
+
+	def hit(self, damage): # méthode pour prendre des dégâts
+		self.life -= damage
+
+	def die(self): # méthode de réaparition
+		self.life = 100
+		self.enemy.addScore(1)
+		self.enemy.addLife(25)
+		self.rect.x, self.rect.y = self.spawnX, self.spawnY
+
+class Block(pygame.sprite.Sprite): # classe bloc
+
+	def __init__(self, x, y, img):
+		super(Block, self).__init__()
+
+		#PARAMETRES
+
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = x, y
+
+class Map(object): #classe map
+
+	def __init__(self, level):
+		super(Map, self).__init__()
+
+		#PARAMETRES
+
+		self.blockList = pygame.sprite.Group()
+		self.level = level
+
+	#METHODES
+
+	def generate(self): # méhode générant un terrain à partir d'un fichier
+		with open(self.level, 'r') as level: #on parcours le fichier
+
+			y = 0
+
+			for lines in level:
+				x = 0
+				for car in lines:
+					if car != '\n': # on ignore les retours à la ligne
+						if car == "#": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+							block = Block(x, y, graphics.block1)
+							self.blockList.add(block)
+						#elif car == "1": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block1)
+						#	self.blockList.add(block)
+						#elif car == "2": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block2)
+						#	self.blockList.add(block)
+						#elif car == "3": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block3)
+						#	self.blockList.add(block)
+						#elif car == "4": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block4)
+						#	self.blockList.add(block)
+						#elif car == "6": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block6)
+						#	self.blockList.add(block)
+						#elif car == "7": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block7)
+						#	self.blockList.add(block)
+						#elif car == "8": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block8)
+						#	self.blockList.add(block)
+						#elif car == "9": # si on trouve le caractère "#" On crée un objet bloc que l'on ajoute à une liste
+						#	block = Block(x, y, block9)
+						#	self.blockList.add(block)
+					x += 32
+				y += 32
+
+class Bullet(pygame.sprite.Sprite): #classe balle
+	bullets = pygame.sprite.Group()
+
+	def __init__(self, x, y, speed, img):
+		super(Bullet, self).__init__()
+
+		#PARAMETRES
+
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = x, y
+		self.speed = speed
+		Bullet.bullets.add(self)
+
+	#METHODES
+
+	@staticmethod
+	def move(): # méthode affectant toutes les instances de la classe Bullet permettant de les faires toutes bouger
+		for bullet in Bullet.bullets:
+			bullet.rect.x += bullet.speed
+
+	def destroy(self): #méthode détruisant l'objet et en le supprimmant de la liste
+		Bullet.bullets.remove(self)
+		del self
+
+class Item(pygame.sprite.Sprite):
+	itemList = pygame.sprite.Group()
+	
+	def __init__(self, x, y, img):
+		super(Item, self).__init__()
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = x, y
+		Item.itemList.add(self)
+
+	def destroy(self):
+		Item.itemList.remove(self)
+		ItemSpawner.available = True
+		del self
+
+class LifeBonus(Item):
+	
+	def __init__(self, x, y, img, life):
+		super(LifeBonus, self).__init__(x, y, img)
+		self.life = life
+	
+	def setLife(self, player):
+		player.addLife(self.life)
+
+
+class ItemSpawner(object):
+
+	available = True
+	
+	def __init__(self, time):
+		super(ItemSpawner, self).__init__()
+		
+		self.time = time
+
+	def spawnItem(self, typ):
+
+		coords = graphics.area[random.randint(0,3)]
+		LifeBonus(coords[0], coords[1], graphics.lifeBonus, 50)
+
+	def update(self, gameTime):
+
+		if gameTime % self.time == 0 and ItemSpawner.available == True:
+			self.spawnItem(1)
+			ItemSpawner.available = False
---
+++ Projet-ISN/process.py
@@ -0,0 +1,105 @@
+import pygame 
+from pygame.locals import *
+from classes import *
+from graphics import *
+from random import randint
+
+def process(player, player2): # fonction gérant les évènements de touche
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+		elif event.type == pygame.KEYDOWN:
+			if event.key == player.right:
+				player.xspeed = player.speed
+				player.image = player.imgR
+				player.isRight = True
+			if event.key == player.left:
+				player.xspeed = -player.speed
+				player.image = player.imgL
+				player.isRight = False
+			if event.key == player.up:
+				if player.yspeed == 0:				
+					player.jump.play()
+					player.yspeed = -(player.speed)*3.5
+			if event.key == player.gun:
+				player.fire.play()
+				if player.isRight == True:
+					player.image = player.imgFR
+					b = Bullet(player.rect.x + player.rect.width, player.rect.y + int(player.rect.height/2) - 4, 18, bullet1)
+				else:
+					player.image = player.imgFL
+					b = Bullet(player.rect.x - 16, player.rect.y + int(player.rect.height/2) - 4, -18, bullet1left)
+
+
+			#PLAYER2----------------------
+
+			if event.key == player2.right:
+				player2.xspeed = player2.speed
+				player2.image = player2.imgR
+				player2.isRight = True
+			if event.key == player2.left:
+				player2.xspeed = -player2.speed
+				player2.image = player2.imgL
+				player2.isRight = False
+			if event.key == player2.up:
+				if player2.yspeed == 0:				
+					player2.jump.play()
+					player2.yspeed = -(player2.speed)*3.5
+			if event.key == player2.gun or event.key == pygame.K_o:
+				player2.fire.play()
+				if player2.isRight == True:
+					player2.image = player2.imgFR
+					b = Bullet(player2.rect.x + player2.rect.width, player2.rect.y + int(player2.rect.height/2) - 4, 18, bullet1)
+				else:
+					player2.image = player2.imgFL
+					b = Bullet(player2.rect.x - 16, player2.rect.y + int(player2.rect.height/2) - 4, -18, bullet1left)
+					
+		elif event.type == pygame.KEYUP:
+			if event.key == player.right:
+				if player.xspeed > 0:
+					player.xspeed = 0
+			if event.key == player.left:
+				if player.xspeed < 0:
+					player.xspeed = 0
+			if event.key == player.gun:
+				if player.isRight == True:
+					player.image = player.imgR
+				else:
+					player.image = player.imgL
+
+			#PLAYER2-------------------------
+
+			if event.key == player2.right:
+				if player2.xspeed > 0:
+					player2.xspeed = 0
+			if event.key == player2.left:
+				if player2.xspeed < 0:
+					player2.xspeed = 0
+			if event.key == player2.gun:
+				if player2.isRight == True:
+					player2.image = player2.imgR
+				else:
+					player2.image = player2.imgL
+
+def collision(player1, player2, levl): # fonction gérant les collisions entre balles/blocs et balles/joueur
+	for b in Bullet.bullets:
+		if pygame.sprite.spritecollide(b, levl.blockList, False):
+			b.destroy()
+
+		elif pygame.sprite.spritecollide(b, player1, False):
+			b.destroy()
+			player1[0].hit(randint(5, 10))
+
+		elif pygame.sprite.spritecollide(b, player2, False):
+			b.destroy()
+			player2[0].hit(randint(5, 10))
+	
+	for item in Item.itemList:
+		if pygame.sprite.spritecollide(item, player1, False):
+			item.setLife(player1[0])
+			item.destroy()
+		elif pygame.sprite.spritecollide(item, player2, False):
+			item.setLife(player2[0])
+			item.destroy()
